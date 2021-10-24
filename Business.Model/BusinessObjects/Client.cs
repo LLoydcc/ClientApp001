73f5177ac6aab67d610e8c301e1a2e4ad737d260
Jan Lüthje <37576268+LLoydcc@users.noreply.github.com>
2021-10-24 20:08:23 +0200

    Add basis structure

---
+++ Business.Model/BusinessObjects/Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp001.Business.Model.BusinessObjects
+{
+    [Serializable]
+    public class Client : INotifyPropertyChanged
+    {
+        public int id;
+        public int Id
+        {
+            get
+            {
+                return id;
+
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private string name;
+        public String Name
+        {
+            get
+            {
+                return name;
+
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(
+                    this,
+                    new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
